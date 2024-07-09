2abe13e0313f6132dd89edcb7cb26b4ac4850f17
rena0dayoungKang <dayoungrenakang@gmail.com>
2024-07-09 14:40:15 +0900

    Create HelloJava.java
    자바수업 day01

---
+++ Exam0709/HelloJava.java
@@ -0,0 +1,15 @@
+//1. 개발환경 설정 및 출력테스트 
+//자바의 확장자는  .java 
+public class HelloJava {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Java!!");
+	}
+
+}
+//영어로 되어 있는 자바 프로그램을 내가 실행할 수 있는 형태로 만들어 주는게 컴파일 
+//이클립스는 저장을 하면 자동으로 컴파일을 해준다. 
+//C:\000\java-workspace\Exam0709\src 
+//C:\000\java-workspace\Exam0709\bin : .class 인 컴파일 형태의 파일이 있음
+//C:\Program Files\Java\jdk-11\bin 에 있는 javac.exe파일이 .java를 .class로 변환을
+//이클립스가 자동으로 해준다. 
